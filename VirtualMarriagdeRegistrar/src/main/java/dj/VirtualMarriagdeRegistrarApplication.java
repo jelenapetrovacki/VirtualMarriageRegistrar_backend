1ec93f22d6983f8f74228aa6b6b5aa3d8a5b7add
jelenapetrovacki <jpetrovacki3@gmail.com>
2021-10-07 11:57:49 +0200

    Initial commit

---
+++ VirtualMarriagdeRegistrar/src/main/java/dj/VirtualMarriagdeRegistrarApplication.java
@@ -0,0 +1,13 @@
+package dj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualMarriagdeRegistrarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualMarriagdeRegistrarApplication.class, args);
+	}
+
+}
