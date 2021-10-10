28f63b453695fcc60fcce881497b87c9f2c91800
jelenapetrovacki <jpetrovacki3@gmail.com>
2021-10-11 01:16:35 +0200

    PUT termin added

--- VirtualMarriagdeRegistrar/src/main/java/dj/ctrls/TerminRestController.java
+++ VirtualMarriagdeRegistrar/src/main/java/dj/ctrls/TerminRestController.java
@@ -3,6 +3,7 @@ package dj.ctrls;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,17 @@ public class TerminRestController {
 	@PostMapping("termin")
 	public Termin insertTermin(@RequestBody Termin termin) {
 		if (!terminRepository.existsById(termin.getId())) {
+
+			Termin terminVracen = terminRepository.save(termin);
+
+			return terminVracen;
+		}
+		return null;
+	}
+	
+	@PutMapping("termin")
+	public Termin updateTermin(@RequestBody Termin termin) {
+		if (terminRepository.existsById(termin.getId())) {
 
 			Termin terminVracen = terminRepository.save(termin);
 
